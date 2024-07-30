1a516e9d90d9e57503a5a041a70bdbabf2084020
Juancaloossx <juancarloossx@gmail.com>
2024-07-30 15:33:03 -0500

    pequeño cambio, es muy dificl hacer el deploy

--- Back/server.js
+++ Back/server.js
@@ -19,20 +19,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Servir archivos estáticos desde la carpeta 'Vista' sirve el index.html en el inicio del servidor
 app.use(express.static(path.join(__dirname,"..", 'Vista'))); //busca el index, porque maneja la solicitud que hacen al servidor y ubica el index y lo pone
 
+
+// Configurar rutas
+
+app.use("/api/login", appLogin);
+app.use("/api/register", appRegister);
+app.use("/api/categoria", appCategoria);
+
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'Vista', 'index.html'));
 });
-// Configurar rutas
-
-//Ruta para manejar el login
-app.use(appLogin);
-
-// Ruta para manejar el registro de usuarios
-app.use(appRegister);
-
-app.use(appCategoria);
-
-
 //const PORT = 3000; // Cambiado a un puerto comúnmente utilizado para servidores web
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
