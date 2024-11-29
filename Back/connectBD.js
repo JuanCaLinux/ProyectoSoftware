b84ad252c8186b291725146dd606640bc74edead
jimenajaime31 <137962056+jimenajaime31@users.noreply.github.com>
2024-11-28 21:24:28 -0500

    Update connectBD.js

--- Back/connectBD.js
+++ Back/connectBD.js
@@ -1,39 +1,16 @@
-// const mysql = require("mysql");
-// const {DB_HOST,DB_NAME,DB_PORT,DB_PASSWORD,DB_USER} = require("./config.js")
-//
-//
-// const db = mysql.createConnection({
-//     host: DB_HOST,
-//     user: DB_USER,
-//     password: DB_PASSWORD, // Aquí deberías especificar la contraseña de tu base de datos MySQL
-//     port:DB_PORT,
-//     database: DB_NAME
-//
-// });
-//
-// db.connect((err) => {
-//     if (err) throw err;
-//     console.log('Conectado a la base de datos MySQL.');
-// });
-//
-// module.exports = db;
-
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2/promise'; // Importa la versión con soporte de promesas
 import { config } from 'dotenv';
-import { parse } from 'url';
 
 config(); // Cargar variables de entorno desde .env
 
 const createDBConnection = async () => {
     try {
-        const dbUrl = process.env.DATABASE_URL;
-        const host = "viaduct.proxy.rlwy.net"
-        const port = 19751
-        const user = "root"
-        const  password = "ArvsjUfSJYLGeRfsLLGRrbnKfvUfgjbI"
-        const database = "railway"
+        const host = process.env.DB_HOST || "localhost";
+        const port = process.env.DB_PORT || 3306;
+        const user = process.env.DB_USER || "root";
+        const password = process.env.DB_PASSWORD || "root";
+        const database = process.env.DB_NAME || "moneymate";
 
-        // mysql://root:ArvsjUfSJYLGeRfsLLGRrbnKfvUfgjbI@viaduct.proxy.rlwy.net:19751/railway
         const db = await mysql.createConnection({
             host,
             port,
@@ -41,6 +18,7 @@ const createDBConnection = async () => {
             password,
             database,
         });
+
         console.log('Conectado a la base de datos MySQL.');
         return db;
     } catch (err) {
@@ -50,4 +28,3 @@ const createDBConnection = async () => {
 };
 
 export default createDBConnection;
-
