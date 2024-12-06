7cf13b62c22264c6cb963650fecbf5dda1770b9d
jimenajaime31 <137962056+jimenajaime31@users.noreply.github.com>
2024-12-06 15:02:52 -0500

    configuraciones para el despliegue
    configuraciones para el despliegue

--- Back/connectBD.js
+++ Back/connectBD.js
@@ -1,23 +1,18 @@
 import mysql from 'mysql2/promise'; // Importa la versión con soporte de promesas
 import { config } from 'dotenv';
 
-config(); // Cargar variables de entorno desde .env
+config(); // Cargar variables de entorno desde .env (local)
 
 const createDBConnection = async () => {
     try {
-        const host = process.env.DB_HOST || "localhost";
-        const port = process.env.DB_PORT || 3306;
-        const user = process.env.DB_USER || "root";
-        const password = process.env.DB_PASSWORD || "root";
-        const database = process.env.DB_NAME || "moneymate";
+        const databaseUrl = process.env.DATABASE_URL; // Usar la URL de la base de datos desde las variables de entorno de Azure
 
-        const db = await mysql.createConnection({
-            host,
-            port,
-            user,
-            password,
-            database,
-        });
+        if (!databaseUrl) {
+            throw new Error('DATABASE_URL no está definida en las variables de entorno');
+        }
+
+        // Desglosar la URL de la base de datos en sus componentes
+        const db = await mysql.createConnection(databaseUrl); // Solo pasar la URL directamente
 
         console.log('Conectado a la base de datos MySQL.');
         return db;
