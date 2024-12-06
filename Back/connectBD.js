1fcb3244e598b2c8385ab48fc34720d58f20d52e
jimenajaime31 <137962056+jimenajaime31@users.noreply.github.com>
2024-12-06 18:27:18 -0500

    porfa ahora sí

--- Back/connectBD.js
+++ Back/connectBD.js
@@ -1,21 +1,17 @@
-import mysql from 'mysql2/promise';  // Usamos mysql2 con promise
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-// Crear la conexión a la base de datos de manera que podamos usar async/await
+dotenv.config(); // Cargar las variables del archivo .env
+
 const createDBConnection = async () => {
     try {
-        // Usamos las variables de entorno configuradas en Vercel
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST,  // Utilizamos la variable de entorno DB_HOST
-            user: process.env.DB_USER,  // Utilizamos la variable de entorno DB_USER
-            password: process.env.DB_PASSWORD,  // Utilizamos la variable de entorno DB_PASSWORD
-            database: process.env.DB_NAME,  // Utilizamos la variable de entorno DB_NAME
-            port: 3306  // El puerto generalmente sigue siendo 3306
-        });
+        const connection = await mysql.createConnection(process.env.DATABASE_URL);
+
         console.log('Conexión a MySQL exitosa');
-        return connection; // Retornamos la conexión
+        return connection;
     } catch (err) {
         console.error('Error al conectar a la base de datos:', err);
-        throw err; // Propagamos el error para manejarlo en el controlador
+        throw err;
     }
 };
 
