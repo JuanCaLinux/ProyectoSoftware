b75f2ba25be2574f64f1a939e2e54b705463af37
jimenajaime31 <137962056+jimenajaime31@users.noreply.github.com>
2024-12-06 16:08:17 -0500

    de vuelta a lo mismo :/
    5000 horas perdidas

--- Back/connectBD.js
+++ Back/connectBD.js
@@ -1,26 +1,26 @@
 import mysql from 'mysql2/promise'; // Importa la versión con soporte de promesas
 import { config } from 'dotenv';
 
-config(); // Cargar variables de entorno desde .env (local)
+config(); // Cargar variables de entorno desde .env
 
 const createDBConnection = async () => {
     try {
-        const databaseUrl = process.env.DATABASE_URL; // Usar la URL de la base de datos desde las variables de entorno de Azure
+        const host = process.env.DB_HOST || "localhost";
+        const port = process.env.DB_PORT || 3306;
+        const user = process.env.DB_USER || "root";
+        const password = process.env.DB_PASSWORD || "root";
+        const database = process.env.DB_NAME || "moneymate";
 
-        if (!databaseUrl) {
-            throw new Error('DATABASE_URL no está definida en las variables de entorno');
-        }
-
-        // Configuración para la conexión con SSL
-        const connection = await mysql.createConnection({
-            uri: databaseUrl,
-            ssl: {
-                ca: fs.readFileSync('/path/to/BaltimoreCyberTrustRoot.crt.pem'), // Ruta al certificado raíz de Azure (puede que lo necesites)
-            }
+        const db = await mysql.createConnection({
+            host,
+            port,
+            user,
+            password,
+            database,
         });
 
-        console.log('Conectado a la base de datos MySQL con SSL.');
-        return connection;
+        console.log('Conectado a la base de datos MySQL.');
+        return db;
     } catch (err) {
         console.error('Error al conectar a la base de datos:', err);
         throw err;
